c0e9ef7c140ba7c6d8747940ce030083f77d4f94
eotobi <eotobi@gmail.com>
2023-05-18 06:35:06 -0700

    fix 7-insert_value

--- 7-insert_value.sql
+++ 7-insert_value.sql
@@ -1,2 +1,6 @@
--- Inserts a new row in the table
-INSERT INTO first_table(id, name) VAlUES(89, 'Coding School');
+-- Get the database name from the command line argument
+SET @database_name := 'hbtn_0c_0';
+
+-- Insert a new row into the first_table
+INSERT INTO @database_name.first_table (id, name) VALUES (89, 'Best School');
+
