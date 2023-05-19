59c228e2db0dad2eb6642748f7e2ccae4892ea9e
eotobi <eotobi@gmail.com>
2023-05-18 22:36:14 -0700

    7-insert_value.sql fix

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,2 +1,2 @@
--- Inserts a new row in the table
-INSERT INTO first_table(id, name) VAlUES(89, 'Coding School');
+-- Insert a new row into the first_table
+INSERT INTO first_table (id, name) VALUES (89, 'Best School');
